b730f33ab2fdf7821ba248789b9c7a34dc2ebe57
The Lood <unilood@gmail.com>
2022-07-15 19:43:23 +1200

    Lesson 2: Layouts

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,26 +3,50 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           MessageCard("Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name:String){
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(id = R.drawable.fam),
+        contentDescription = "Family Photo",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard(){
-    MessageCard("Android")
+fun PreviewMessageCard() {
+    MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
